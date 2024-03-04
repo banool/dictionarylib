d8e90030bb17d8760ea793b230942e8b18307d59
Daniel Porteous <daniel@dport.me>
2024-03-04 22:08:31 +0000

    Cautiously do not throw if entriesGlobal is empty for now

--- lib/globals.dart
+++ lib/globals.dart
@@ -153,21 +153,24 @@ Future<void> setupPhaseTwo(
   // This depends on the knob values above being set so it is important that
   // this appears after that block above and after any knobs set between phases
   // one and two.
+  // TODO: entriesGlobalReplacement doesn't work well here, all the vars that
+  // depend on it won't get populated.
   if (downloadWordsData && entriesGlobalReplacement == null) {
     if (entriesGlobal.isEmpty) {
       printAndLog(
           "No local entry data cache found, fetching updates from the internet and waiting for them before proceeeding...");
       await paramEntryLoader.updateWordsData(true);
-      if (entriesGlobal.isEmpty) {
-        printAndLog(
-            "entriesGlobal is after the loading phase where no local data was found, throwing...");
-        throw "Global entries data is empty after the data loading phase where no local data was found. This is a bug, please report it.";
-      }
     } else {
       printAndLog(
           "Local entry data cache found, fetching updates from the internet in the background...");
       paramEntryLoader.updateWordsData(false);
     }
+  }
+
+  if (entriesGlobal.isEmpty) {
+    // TODO: Make this throw later.
+    printAndLog(
+        "entriesGlobal is after the loading phase where no local data was found. This should be a bug but we are continuing for now...");
   }
 
   entryLoader = paramEntryLoader;
